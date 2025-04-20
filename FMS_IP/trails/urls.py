2b968c5325e097cf87d7fc32f84efe789a8ab5a0
Vebeta <vladkhegay19@gmail.com>
2025-04-20 23:44:57 +0700

    Created registration form. Remade user like inner class. (Next step - remove user app) . Also redefined all staff, connected with user

--- FMS_IP/trails/urls.py
+++ FMS_IP/trails/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', TrailsHome.as_view(), name='trails'),
-    path('trail/<int:trl_id>', trail_page, name='trail'),
-    path('addtrail', add_trail, name='addtrail')
+    path('trail/<slug:trl_slug>', TrailPage.as_view(), name='trail'),
+    path('addtrail', AddTrail.as_view(), name='addtrail')
 ]
--- FMS_IP/trails/views.py
+++ FMS_IP/trails/views.py
@@ -1,43 +1,41 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView
+from django.urls import reverse_lazy
 
 from .models import *
 from .forms import *
+from .utils import *
 
 
-class TrailsHome(ListView):
+class TrailsHome(DataMixin, ListView):
     model = Trail
     template_name = 'trails/trails.html'
     context_object_name = 'obj'
+    paginate_by = 1
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Маршруты'
-        return context
+        uniq_ctxt = self.get_user_context(title='Маршруты')
+        return dict(list(context.items()) + list(uniq_ctxt.items()))
 
     def get_queryset(self):
         return Trail.objects.filter(is_published=True)
 
 
-def trail_page(request, trl_id):
-    trail = get_object_or_404(Trail, pk=trl_id)
-    data = {
-        'title': trail.title,
-        'trail': trail
-    }
-    return render(request, 'trails/trail.html', data)
+class TrailPage(DataMixin, DetailView):
+    model = Trail
+    template_name = 'trails/trail_page.html'
+    slug_url_kwarg = 'trl_slug'
+    context_object_name = 'trail'
 
-def add_trail(request):
-    if request.method == 'POST':
-        print('ok')
-        form = AddTrailForm(request.POST)
-        if form.is_valid():
-            try:
-                form.save()
-                return redirect('trails')
-            except:
-                form.add_error(None, 'Ошибка добавления маршрута')
-    else:
-        print('bl')
-        form = AddTrailForm()
-    return render(request, 'trails/add_trail.html', {'title': 'Добавление маршрута', 'form': form})
+
+class AddTrail(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddTrailForm
+    template_name = 'trails/add_trail.html'
+    login_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        uniq_ctxt = self.get_user_context(title='Добавление маршрута')
+        return dict(list(context.items()) + list(uniq_ctxt.items()))
